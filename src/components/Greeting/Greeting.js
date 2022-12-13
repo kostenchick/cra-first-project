33aa3308603a38a01982efbe5f37f6cc10b534be
kostenchcick <kostenchick1211@gmail.com>
2022-12-13 18:25:27 +0100

    Greeting state

--- src/components/Greeting/Greeting.js
+++ src/components/Greeting/Greeting.js
@@ -3,11 +3,22 @@ import React from "react";
 class Greeting extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            isGreeting: true
+        }
+    }
+
+    ClickHandler() {
+        this.setState({
+            isGreeting: !this.state.isGreeting
+        })
     }
 
     render() {
-        return <h1>Hello {this.props.name} from {this.props.country}!</h1>
+        const {isGreeting} = this.state;
+        const greetingText = isGreeting ? 'Hello' : 'Bye';
+
+        return <h1 onClick={() =>{this.ClickHandler()}}>{greetingText} {this.props.name} from {this.props.country}!</h1>
     }
 
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App/App';
 import Greeting from './components/Greeting/Greeting';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
