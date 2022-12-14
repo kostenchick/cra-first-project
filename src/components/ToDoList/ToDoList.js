4eea977937aefc8d38c0f2d32ecb3fe3c08851b6
kostenchcick <kostenchick1211@gmail.com>
2022-12-14 18:57:51 +0100

    ToDoList task resolve (maximum lvl)

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -24,9 +24,20 @@ class ToDoList extends React.Component {
         }; 
     }
 
+    removeTask(taskIDtoRemove) {
+        const {todoList} = this.state;
+        const filteredArray = todoList.filter(task => task.id !== taskIDtoRemove);
+        this.setState({
+            todoList: filteredArray
+        })
+    }
+
     renderLi() {
         const {todoList} = this.state;
-        return todoList.map((task) => <li key={task.id}>{task.text}</li>)
+        return todoList.map((task) => <li key={task.id}>
+            {task.text}
+            <button onClick={() =>{this.removeTask(task.id)}}>X</button>
+        </li>)  
     }
 
     render() {
